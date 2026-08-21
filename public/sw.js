0b0883ea9c43b1cc81f869cd080633d6e5e97222
lsnieg-design <lsnieg@raspanti.edu.ar>
2026-08-21 11:54:03 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'centra-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
